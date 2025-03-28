f62f89d33ded7ca03f9daf7f3cec3ede25e12e1d
cho-seung-hoon <whtmdgns70@gmail.com>
2025-03-28 17:18:06 +0900

    20250328 기본

---
+++ 20250328/router-test/src/pages/About.vue
@@ -0,0 +1,50 @@
+<!-- <template>
+  <div class="card card-body">
+    <h2>About</h2>
+    <p>요청 경로 : {{ $route.fullPath }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'About',
+  created() {
+    console.log(this.$route.fullPath);
+  },
+};
+</script> -->
+
+<!-- <template>
+  <div class="card card-body">
+    <h2>About</h2>
+    <p>요청 경로 setup : {{ url }}</p>
+  </div>
+</template>
+<script>
+import { useRoute } from 'vue-router';
+export default {
+  name: 'About',
+  setup() {
+    const url = useRoute().fullPath;
+
+    return { url };
+  },
+};
+</script> -->
+
+<template>
+  <div class="card card-body">
+    <h2>About</h2>
+    <p>요청 경로 setup : {{ url }}</p>
+  </div>
+</template>
+<script>
+import { useRoute } from 'vue-router';
+export default {
+  name: 'About',
+  setup() {
+    const url = useRoute().fullPath;
+
+    return { url };
+  },
+};
+</script>
---
+++ 20250328/router-test/src/pages/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card card-body">
+    <h2>Test</h2>
+    <div>
+      <router-link :to="{ name: 'test/num', params: { num: 1 } }">
+        <button class="btn btn-primary">1번 버튼</button>
+      </router-link>
+    </div>
+    <!-- 라우트된 컴포넌트가 보여지는 부분 -->
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Test',
+};
+</script>
